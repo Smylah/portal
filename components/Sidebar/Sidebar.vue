aece98112be2d3690de4ea992b8cc52c819c591c
Oghenetega Esedere <arcteggzz@gmail.com>
2022-09-06 19:37:38 +0000

    test

---
+++ components/Sidebar/Sidebar.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+import layouts from '~/mixins/layouts'
+
+export default defineComponent({
+    name: 'Sidebar',
+
+    components: {},
+    mixins: [layouts],
+
+    props: {
+    },
+
+    setup(_props) {},
+
+    head() {
+        return {
+            title: '',
+            meta: [
+                {
+                    hid: '',
+                    name: '',
+                    content: '',
+                },
+            ],
+        }
+    },
+})
+</script>
+
+<template>
+    <div>Sidebar</div>
+</template>
---
+++ layouts/index.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+import layouts from '~/mixins/layouts'
+import Sidebar from '../components/Sidebar/Sidebar.vue'
+
+export default defineComponent({
+    name: 'DefaultLayout',
+
+    components: {Sidebar},
+    mixins: [layouts],
+
+    props: {
+    },
+
+    setup(_props) {},
+
+    head() {
+        return {
+            title: 'Home',
+            meta: [
+                {
+                    hid: '',
+                    name: '',
+                    content: '',
+                },
+            ],
+        }
+    },
+})
+</script>
+
+<template>
+    <div>
+        <Sidebar />
+        <NuxtChild />
+    </div>
+</template>
