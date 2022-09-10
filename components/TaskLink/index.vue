6e5ebec8cfa089ef97fe964e1a252c7558c3eecb
Oghenetega Esedere <arcteggzz@gmail.com>
2022-09-10 01:49:51 +0100

    complete setup page initial start

---
+++ components/TaskLink/index.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import { defineComponent } from '@vue/composition-api'
+    import layouts from '~/mixins/layouts'
+
+    
+    export default defineComponent({
+        name: 'TaskLinkComponent',
+    
+        components: {},
+        mixins: [layouts],
+    
+        props: {
+            TaskName: {
+                type: String
+            },
+            TaskLink: {
+                type: String
+            }
+        },
+    
+        setup(_props) {},
+
+    })
+</script>
+    
+<template>
+    <NuxtLink class="flex justify-between items-center bg-white pl-[1.35rem] py-[1.2rem] pr-[1.65rem]" :to="TaskLink">
+        <div class="flex items-center">
+            <div class="bg-black w-[14px] h-[14px] rounded-full"></div>
+            <p>{{TaskName}}</p>
+        </div>
+        <div class="bg-black w-[14px] h-[11px]"></div>
+    </NuxtLink>
+</template>
+    
---
+++ pages/complete-setup/index.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import { defineComponent } from '@vue/composition-api'
+    import TaskLink from '../../components/TaskLink/index.vue'
+    
+    export default defineComponent({
+        name: 'CompleteSetupPage',
+        components: {TaskLink},
+        layout: 'talent',
+        transition: 'page-transition-slide-left',
+        setup() {},
+        
+        head: () => ({
+            titleTemplate: '',
+            title: '',
+        }),
+    })
+</script>
+    
+<template>
+    <div class="">
+        <div class="px-[4.5rem] pt-[2.5rem]">
+            <h2 class="text-[1.75rem] leading-[2.25rem] font-medium mb-[0.8rem]">Welcome to UnbugQA!</h2>
+            <p class="text-[1.2rem] leading-[1.75rem] font-normal text-[#374151]">Hello John! Let’s get you set to start hiring awesome QA Engineers for your company.</p>
+        </div>
+        <div>
+            <p>required for creating your first gig</p>
+            <TaskLink TaskLink="/" TaskName="Complete your company profile" />
+        </div>
+    </div>
+</template>
+
+<style lang="postcss"></style>
+    
